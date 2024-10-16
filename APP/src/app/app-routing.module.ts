03b151cee60b0ab47f518b95e77a4e3f9d7ea018
vijaybyagari <vijaybyagari06@gmail.com>
2024-10-16 15:40:35 +0530

    implemeted sidenav

--- APP/src/app/app-routing.module.ts
+++ APP/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OrderHistoryComponent } from './components/order-history/order-history.component';
+import { NotificationsComponent } from './components/notifications/notifications.component';
+import { NewOrdersComponent } from './components/new-orders/new-orders.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",
+    redirectTo:"app",
+    pathMatch:"full"
+  },
+  {
+    path:"orderhistory",
+    component:OrderHistoryComponent
+  },
+  {
+    path:"Notifications",
+    component:NotificationsComponent
+  },
+  {
+    path:"neworders",
+    component:NewOrdersComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- APP/src/app/app.module.ts
+++ APP/src/app/app.module.ts
@@ -4,14 +4,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { OrderHistoryComponent } from './components/order-history/order-history.component';
+import { NotificationsComponent } from './components/notifications/notifications.component';
+import { NewOrdersComponent } from './components/new-orders/new-orders.component';
+
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrderHistoryComponent,
+    NotificationsComponent,
+    NewOrdersComponent
+    
   ],
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule
+    
+
   ],
   providers: [
     provideAnimationsAsync()
